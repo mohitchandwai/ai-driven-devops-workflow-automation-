28da249b98c63e5abe21f83d47d72e2469ca78ad
Mohit chandwani <chandwanimohit87m@gmail.com>
2025-11-07 13:06:09 +0530

    change content

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> rajit loda
+          Edit <code>src/App.js</code> mohit
         </p>
         <a
           className="App-link"
